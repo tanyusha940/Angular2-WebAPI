6335ad7842c5184c7a79fbd48fd52d10ac1943da
tanyusha940 <tanyusha_shkoda@mail.ru>
2017-10-30 17:39:38 +0300

    Create database

---
+++ TestTask/Models/Basket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestTask.Models
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public Basket()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
---
+++ TestTask/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestTask.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Basket> Baskets { get; set; }
+        public Product()
+        {
+            Baskets = new List<Basket>();
+        }
+    }
+}
