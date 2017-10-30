ca550692e21a8a192fc02b893ed7e99c74278fda
tanyusha940 <tanyusha_shkoda@mail.ru>
2017-10-30 18:44:59 +0300

    created ProductController and initialize started values

---
+++ TestTask/Controllers/ProductsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TestTask.Models;
+
+namespace TestTask.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private ProductContext db = new ProductContext();
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+
+
+        public Product GetProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            return product;
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(void))]
+        public void CreateProduct([FromBody]Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+        }
+
+        [HttpPut]
+        [ResponseType(typeof(Product))]
+        public void EditBook(int id, [FromBody]Product product)
+        {
+            if (id == product.Id)
+            {
+                db.Entry(product).State = EntityState.Modified;
+
+                db.SaveChanges();
+            }
+        }
+
+        
+        [ResponseType(typeof(Product))]
+        public void DeleteProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                NotFound();
+            }
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TestTask/Models/Initializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TestTask.Models
+{
+    public class Initializer : DropCreateDatabaseAlways<ProductContext>
+    {
+        protected override void Seed(ProductContext db)
+        {
+            Product product_1 = new Product {Name = "Mango", Description = "Fruit", Price = 15.8f};
+            Product product_2 = new Product { Name = "Apple", Description = "Fruit", Price = 15.8f };
+            Product product_3 = new Product { Name = "Tomato", Description = "Vegetables", Price = 15.8f };
+            db.Products.AddRange(new List<Product> {product_1, product_2,product_3});
+
+            Basket basket_1 = new Basket{Name = "BasketWithFrut"};
+            basket_1.Products.Add(product_1);
+            basket_1.Products.Add(product_2);
+            Basket basket_2 = new Basket { Name = "BasketWithVegetables" };
+            basket_2.Products.Add(product_3);
+            db.SaveChanges();
+        }
+
+    }
+}
