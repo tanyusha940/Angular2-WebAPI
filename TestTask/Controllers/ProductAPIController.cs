3906d27633b680aa8ae8293f33d21d683a91c3e4
tanyusha940 <tanyusha_shkoda@mail.ru>
2017-10-31 10:31:10 +0300

    Created ProductAPIController

---
+++ TestTask/Controllers/ProductAPIController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestTask.Models;
+
+namespace TestTask.Controllers
+{
+    public class ProductAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(productContext.Products.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]Product value)
+        {
+            productContext.Products.Add(value);
+            return ToJson(productContext.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]Product value)
+        {
+            productContext.Entry(value).State = EntityState.Modified;
+            return ToJson(productContext.SaveChanges());
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            productContext.Products.Remove(productContext.Products.FirstOrDefault(x => x.Id == id));
+            return ToJson(productContext.SaveChanges());
+        }
+    }
+}
