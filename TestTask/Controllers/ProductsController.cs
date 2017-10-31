bfaecf266cad62c58efce2108eb9e2c646cc10e6
tanyusha940 <tanyusha_shkoda@mail.ru>
2017-10-31 08:47:43 +0300

    Сhange controllers. It's working:)

--- TestTask/Controllers/ProductsController.cs
+++ TestTask/Controllers/ProductsController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using TestTask.Models;
+using TestTask.ViewModels;
 
 namespace TestTask.Controllers
 {
@@ -21,29 +16,56 @@ namespace TestTask.Controllers
             unitOfWork = new UnitOfWork();
         }
 
-        public IQueryable<Product> GetBaskets()
+        public IEnumerable<Product> GetProducts()
         {
             return unitOfWork.Product.GetAll();
         }
 
-        public Product GetBasket(int id)
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult GetProduct(int id)
         {
-            Product basket = unitOfWork.Product.Get(id);
-            return basket;
+            Product product = unitOfWork.Product.Get(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
         }
 
         [HttpPost]
         [ResponseType(typeof(void))]
-        public void CreateProduct([FromBody]Product product)
+        public IHttpActionResult CreateProduct([FromBody] CreateProductViewModel product)
         {
-            unitOfWork.Product.Create(product);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            unitOfWork.Product.Create(new Product
+            {
+                Name = product.Name,
+                Price = product.Price,
+                Description = product.Description
+            });
+            unitOfWork.Save();
+            return Ok();
         }
 
         [HttpPut]
-        [ResponseType(typeof(Product))]
-        public void EditBasket(int id, [FromBody]Product product)
+        [ResponseType(typeof(void))]
+        public IHttpActionResult EditProduct([FromBody]EditProductViewModel product)
         {
-            unitOfWork.Product.Update(product);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            unitOfWork.Product.Update(new Product
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description
+            });
+            unitOfWork.Save();
+            return Ok();
         }
 
         [HttpDelete]
@@ -51,6 +73,7 @@ namespace TestTask.Controllers
         public void DeleteProduct(int id)
         {
             unitOfWork.Product.Delete(id);
+            unitOfWork.Save();
         }
 
         protected override void Dispose(bool disposing)
